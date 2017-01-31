38a622e1cefc38b4493ceaec19c9a4345d1fead2
jacksoncooper <jacksoncooper@users.noreply.github.com>
2017-01-31 09:23:37 -0800

    Add calculate() function test.

--- tests.js
+++ tests.js
@@ -1,16 +1,20 @@
-QUnit.test("Test the displayCurrentInput function.", function (assert) {
+QUnit.test("Test the displayCurrentInput() function.", function (assert) {
     currentInput = 3245234
-
     displayCurrentInput();
     assert.equal(document.getElementById('screen').value, "3245234", "We expect the value to be 3245234.");
-
     currentInput = -53245342523453453453453453543453425345345345346546746
-
     displayCurrentInput();
     assert.equal(document.getElementById('screen').value, "-53245342523453453453453453543453425345345345346546746", "We expect the result to be 53245342523453453453453453543453425345345345346546746 in exponential form");
-
     currentInput = -.90009980943750987
-
     displayCurrentInput();
     assert.equal(document.getElementById('screen').value, "-.90009980943750987", "We expect the result to be -.90009980943750987.");
+});
+QUnit.test("Test the calculate() function.", function (assert) {
+    allClear();
+    memory = ['1', '+', '2', '-', '3'];
+    calculate();
+    assert.deepEqual(document.getElementById('screen').value, "0", "We expect the result to be 0.");
+    allClear();
+    memory = ['1', '+', '2', '*', '3'];
+    assert.deepEqual(document.getElementById('screen').value, "7", "We expect the result to be 7.");
 });
