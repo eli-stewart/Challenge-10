c4e3e4fd812d3387c8e6271cb9ff274b2ed0d6f6
Eli W Stewart <es04832@srcs.loc>
2017-02-02 08:24:21 -0800

    fixed percentage test

--- tests.js
+++ tests.js
@@ -5,9 +5,9 @@ QUnit.test("Test the displayCurrentInput() function.", function (assert) {
     currentInput = -53245342
     displayCurrentInput();
     assert.equal(document.getElementById('screen').value, "-53245342", "We expect the result to be -53245342");
-    currentInput = -.90009980943750987
+    currentInput = -.9000998094375
     displayCurrentInput();
-    assert.equal(document.getElementById('screen').value, "-.90009980943750987", "We expect the result to be -.90009980943750987.");
+    assert.equal(document.getElementById('screen').value, "-.9000998094375", "We expect the result to be -.9000998094375.");
 });
 QUnit.test("Test the calculate() function.", function (assert) {
     allClear();
@@ -22,7 +22,7 @@ QUnit.test("Test the calculate() function.", function (assert) {
 QUnit.test("Test the percentage function", function (assert) {
     currentInput = 23
     percentage();
-    assert.equal(document.getElementById('screen').value, ".23", "We expect the value to be .23")
+    assert.equal(document.getElementById('screen').value, "0.23", "We expect the value to be 0.23")
     currentInput = 450
     percentage();
     assert.equal(document.getElementById('screen').value, "4.5", "We expect the value to be 4.5")
