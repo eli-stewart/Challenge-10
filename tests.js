e96ac8e68c908610880c6e0585c594ccdd24d32f
Eli W Stewart <es04832@srcs.loc>
2017-02-02 09:17:45 -0800

    invrse test 2

--- tests.js
+++ tests.js
@@ -64,8 +64,8 @@ QUnit.test("Test the squareRoot function", function (assert) {
 QUnit.test("Test the inverse function", function (assert) {
     currentInput = 64;
     inverse();
-    assert.equal(document.getElementById('screen').value, '1/64', 'We expect the value 8')
+    assert.equal(document.getElementById('screen').value, '0.015625', 'We expect the value 8')
  currentInput = -100897908790874;
     squareRoot();
-    assert.equal(document.getElementById('screen').value, 'NaN', 'We expect the value NaN');
+    assert.equal(document.getElementById('screen').value, '321423', 'We expect the value NaN');
 });
